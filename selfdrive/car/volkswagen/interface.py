e00d0cd435d1fd2937c9ba24b14696e07ba79006
Edwin Kinara <edukinara@users.noreply.github.com>
2020-01-22 15:49:47 -0600

    tune

--- selfdrive/car/volkswagen/interface.py
+++ selfdrive/car/volkswagen/interface.py
@@ -57,7 +57,7 @@ class CarInterface(CarInterfaceBase):
       ret.steerControlType = car.CarParams.SteerControlType.torque
 
       # Additional common MQB parameters that may be overridden per-vehicle
-      ret.steerRateCost = 1.0
+      ret.steerRateCost = 0.6
       ret.steerActuatorDelay = 0.05 # Hopefully all MQB racks are similar here
       ret.steerLimitTimer = 0.4
       ret.steerMaxBP = [0.]  # m/s
