9e2985de5abac2882bd88590575953eb80511f86
Edwin Kinara <edwinkinara@gmail.com>
2020-03-24 11:41:57 -0500

    Cleanup

--- selfdrive/controls/lib/pathplanner.py
+++ selfdrive/controls/lib/pathplanner.py
@@ -79,7 +79,6 @@ class PathPlanner():
     self.angle_steers_des_time = 0.0
 
   def update(self, sm, pm, CP, VM):
-
     v_ego = sm['carState'].vEgo
     angle_steers = sm['carState'].steeringAngle
     active = sm['controlsState'].active
